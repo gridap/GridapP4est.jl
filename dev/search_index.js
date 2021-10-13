37bab44ca7d1a4064997a064cbdae5367a190aed
Documenter.jl <documenter@juliadocs.github.io>
2021-10-13 04:08:24 +0000

    build based on 12ddda4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GridapP4est","category":"page"},{"location":"#GridapP4est","page":"Home","title":"GridapP4est","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GridapP4est.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GridapP4est]","category":"page"}]
+}
