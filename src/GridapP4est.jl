f81756cfd65c535a09a91dfcedb1f1760ca567af
amartin <alberto.martin@monash.edu>
2021-10-30 18:22:54 +1100

    Renaming

--- src/GridapP4est.jl
+++ src/GridapP4est.jl
@@ -6,5 +6,6 @@ module GridapP4est
   using P4est_wrapper
   const PArrays = PartitionedArrays
   include("UniformlyRefinedForestOfOctreesDiscreteModels.jl")
+  include("CubedSphereDistributedDiscreteModels.jl")
   export UniformlyRefinedForestOfOctreesDiscreteModel
 end
