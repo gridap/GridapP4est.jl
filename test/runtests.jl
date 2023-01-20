4bad9529ae8d55d642b322738d3bb51dc1ae39e2
JordiManyer <j.manyer@hotmail.com>
2023-01-20 16:18:30 +0100

    Added failing case as example

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,7 +31,7 @@ function run_tests(testdir)
     testfiles = sort(filter(istest, readdir(testdir)))
     @time @testset "$f" for f in testfiles
       MPI.mpiexec() do cmd
-        if f in ["UniformlyRefinedForestOfOctreesDiscreteModelsTests.jl"]
+        if f in ["UniformlyRefinedForestOfOctreesDiscreteModelsTests.jl","NoEnvironmentTests.jl"]
           np = 4
           extra_args = "-s 2 2 -r 2"
         elseif f in ["OctreeDistributedDiscreteModelsTests.jl"]
