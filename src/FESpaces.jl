0131bd64bb728f751e1bb4c891d29cf999bdea5e
Alberto F. Martin <alberto.martin@monash.edu>
2023-09-10 19:59:45 +1000

    Using again the expected _coarse_faces_to_child_ids_3D[:]

--- src/FESpaces.jl
+++ src/FESpaces.jl
@@ -22,7 +22,7 @@ function _generate_face_subface_ldof_to_cell_ldof(Df,Dc,reffe::Tuple{<:Lagrangia
 end 
 
 const _coarse_faces_to_child_ids_2D=[1 2; 3 4; 1 3; 2 4]
-const _coarse_faces_to_child_ids_3D=[1 3 2 4; 5 7 6 8; 1 2 5 6; 3 4 7 8; 1 3 5 7; 2 4 6 8]
+const _coarse_faces_to_child_ids_3D=[1 2 3 4; 5 6 7 8; 1 3 5 7; 2 4 6 8; 1 2 5 6; 3 4 7 8]
 
 function _generate_unit_hypercube_model(Dc)
     @assert Dc==2 || Dc==3
