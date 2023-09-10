9e8ba7f2e1887118e037a2b8f731a680b4f88fb1
Alberto F. Martin <alberto.martin@monash.edu>
2023-09-10 20:47:50 +1000

    More work into DarcyNonConformingTests

--- test/DarcyNonConformingOctreeModelsTests.jl
+++ test/DarcyNonConformingOctreeModelsTests.jl
@@ -10,209 +10,141 @@ module DarcyNonConformingOctreeModelsTests
   using Logging
   using Test
 
-#   function test_transfer_ops_and_redistribute(ranks,dmodel,order)
-#     # Define manufactured functions
-#     u(x) = x[1]+x[2]^order
-#     f(x) = -Δ(u)(x)
-#     degree = 2*order+1
-#     reffe=ReferenceFE(lagrangian,Float64,order)
-#     VH=FESpace(dmodel,reffe,conformity=:H1;dirichlet_tags="boundary")
-#     UH=TrialFESpace(VH,u)
-#     ref_coarse_flags=map(ranks,partition(get_cell_gids(dmodel.dmodel))) do rank,indices
-#         flags=zeros(Cint,length(indices))
-#         flags.=nothing_flag
+  function test_transfer_ops_and_redistribute(ranks,
+                                              dmodel::GridapDistributed.DistributedDiscreteModel{Dc},
+                                              order) where Dc
+    ref_coarse_flags=map(ranks,partition(get_cell_gids(dmodel.dmodel))) do rank,indices
+        flags=zeros(Cint,length(indices))
+        flags.=nothing_flag
         
-#         flags[1]=refine_flag
-#         flags[own_length(indices)]=refine_flag
+        flags[1]=refine_flag
+        flags[own_length(indices)]=refine_flag
 
-#         # To create some unbalance
-#         if (rank%2==0 && own_length(indices)>1)
-#             flags[div(own_length(indices),2)]=refine_flag
-#         end
-#         flags
-#     end 
-#     fmodel,glue=adapt(dmodel,ref_coarse_flags);
-#     # map(ranks,glue) do rank, glue 
-#     #   if rank==2
-#     #     print(glue.n2o_faces_map[end]); print("\n")
-#     #   end  
-#     # end
-#     Vh=FESpace(fmodel,reffe,conformity=:H1;dirichlet_tags="boundary")
-#     Uh=TrialFESpace(Vh,u)
+        # To create some unbalance
+        if (rank%2==0 && own_length(indices)>1)
+            flags[div(own_length(indices),2)]=refine_flag
+        end
+        flags
+    end 
+    fmodel,glue=adapt(dmodel,ref_coarse_flags);
+    
+    # Solve coarse
+    xH,XH=solve_darcy(dmodel,order)
+    check_error_darcy(dmodel,order,xH)
+    uH,pH=xH
+    UH,PH=XH
 
-#     ΩH  = Triangulation(dmodel)
-#     dΩH = Measure(ΩH,degree)
+    # Solve fine
+    xh,Xh=solve_darcy(fmodel,order)
+    check_error_darcy(fmodel,order,xh)
+    uh,ph=xh
+    Uh,Ph=Xh
 
-#     aH(u,v) = ∫( ∇(v)⊙∇(u) )*dΩH
-#     bH(v) = ∫(v*f)*dΩH
+    Ωh = Triangulation(fmodel)
+    degree = 2*(order+1)
+    dΩh = Measure(Ωh,degree)
 
-#     op = AffineFEOperator(aH,bH,UH,VH)
-#     uH = solve(op)
-#     e = u - uH
+    # prolongation via interpolation
+    uHh=interpolate(uH,Uh)   
+    e = uh - uHh
+    el2 = sqrt(sum( ∫( e⋅e )*dΩh ))
+    tol=1e-6
+    println("[INTERPOLATION] el2 < tol: $(el2) < $(tol)")
+    @assert el2 < tol
 
-#     # # Compute errors
-#     el2 = sqrt(sum( ∫( e*e )*dΩH ))
-#     eh1 = sqrt(sum( ∫( e*e + ∇(e)⋅∇(e) )*dΩH ))
+    # prolongation via L2-projection 
+    # Coarse FEFunction -> Fine FEFunction, by projection
+    ahp(u,v)  = ∫(v⋅u)*dΩh
+    lhp(v)    = ∫(v⋅uH)*dΩh
+    oph      = AffineFEOperator(ahp,lhp,Uh,Uh)
+    uHh      = solve(oph)
+    e = uh - uHh
+    el2 = sqrt(sum( ∫( e⋅e )*dΩh ))
+    println("[L2 PROJECTION] el2 < tol: $(el2) < $(tol)")
+    @assert el2 < tol
 
-#     tol=1e-6
-#     println("[SOLVE COARSE] el2 < tol: $(el2) < $(tol)")
-#     println("[SOLVE COARSE] eh1 < tol: $(eh1) < $(tol)")
-#     @assert el2 < tol
-#     @assert eh1 < tol
+    # restriction via interpolation
+    uhH=interpolate(uh,UH) 
+    e = uH - uhH
+    el2 = sqrt(sum( ∫( e⋅e )*dΩh ))
+    println("[INTERPOLATION] el2 < tol: $(el2) < $(tol)")
+    @assert el2 < tol
 
+    # restriction via L2-projection
+    ΩH = Triangulation(dmodel)
+    degree = 2*(order+1)
+    dΩH = Measure(ΩH,degree)
+    
+    dΩhH = Measure(ΩH,Ωh,2*order)
+    aHp(u,v) = ∫(v⋅u)*dΩH
+    lHp(v)   = ∫(v⋅uh)*dΩhH
+    oph     = AffineFEOperator(aHp,lHp,UH,UH)
+    uhH     = solve(oph)
+    e       = uH - uhH
+    el2     = sqrt(sum( ∫( e⋅e )*dΩH ))
 
-#     Ωh  = Triangulation(fmodel)
-#     dΩh = Measure(Ωh,degree)
-
-#     ah(u,v) = ∫( ∇(v)⊙∇(u) )*dΩh
-#     bh(v) = ∫(v*f)*dΩh
-
-#     op = AffineFEOperator(ah,bh,Uh,Vh)
-#     uh = solve(op)
-#     e = u - uh
-
-#     writevtk(ΩH, "ctrian", cellfields=["uH"=>uH])
-#     writevtk(Ωh, "ftrian", cellfields=["uh"=>uh])
-
-#     # # Compute errors
-
-#     el2 = sqrt(sum( ∫( e*e )*dΩh ))
-#     eh1 = sqrt(sum( ∫( e*e + ∇(e)⋅∇(e) )*dΩh ))
- 
-#     println("[SOLVE FINE] el2 < tol: $(el2) < $(tol)")
-#     println("[SOLVE FINE] eh1 < tol: $(eh1) < $(tol)")
-#     @assert el2 < tol
-#     @assert eh1 < tol
-
-#     # prolongation via interpolation
-#     uHh=interpolate(uH,Uh)   
-#     e = uh - uHh
-#     el2 = sqrt(sum( ∫( e*e )*dΩh ))
-#     tol=1e-6
-#     println("[INTERPOLATION] el2 < tol: $(el2) < $(tol)")
-#     @assert el2 < tol
-
-#     # prolongation via L2-projection 
-#     # Coarse FEFunction -> Fine FEFunction, by projection
-#     ahp(u,v)  = ∫(v⋅u)*dΩh
-#     lhp(v)    = ∫(v⋅uH)*dΩh
-#     oph      = AffineFEOperator(ahp,lhp,Uh,Vh)
-#     uHh      = solve(oph)
-#     e = uh - uHh
-#     el2 = sqrt(sum( ∫( e*e )*dΩh ))
-#     println("[L2 PROJECTION] el2 < tol: $(el2) < $(tol)")
-#     @assert el2 < tol
-
-#     # restriction via interpolation
-#     uhH=interpolate(uh,UH) 
-#     e = uH - uhH
-#     el2 = sqrt(sum( ∫( e*e )*dΩh ))
-#     println("[INTERPOLATION] el2 < tol: $(el2) < $(tol)")
-#     @assert el2 < tol
-
-#     # restriction via L2-projection
-#     dΩhH = Measure(ΩH,Ωh,2*order)
-#     aHp(u,v) = ∫(v⋅u)*dΩH
-#     lHp(v)   = ∫(v⋅uh)*dΩhH
-#     oph     = AffineFEOperator(aHp,lHp,UH,VH)
-#     uhH     = solve(oph)
-#     e       = uH - uhH
-#     el2     = sqrt(sum( ∫( e*e )*dΩH ))
-
-#     fmodel_red, red_glue=GridapDistributed.redistribute(fmodel);
-#     Vhred=FESpace(fmodel_red,reffe,conformity=:H1;dirichlet_tags="boundary")
-#     Uhred=TrialFESpace(Vhred,u)
-
-#     Ωhred  = Triangulation(fmodel_red)
-#     dΩhred = Measure(Ωhred,degree)
-
-#     ahred(u,v) = ∫( ∇(v)⊙∇(u) )*dΩhred
-#     bhred(v)   = ∫(v*f)*dΩhred
-
-#     op    = AffineFEOperator(ahred,bhred,Uhred,Vhred)
-#     uhred = solve(op)
-#     e = u - uhred
-#     el2 = sqrt(sum( ∫( e*e )*dΩhred ))
-#     println("[SOLVE FINE REDISTRIBUTED] el2 < tol: $(el2) < $(tol)")
-#     @assert el2 < tol
+    fmodel_red, red_glue=GridapDistributed.redistribute(fmodel);
+    xh_red,Xh_red=solve_darcy(fmodel_red,order)
+    check_error_darcy(fmodel_red,order,xh_red)
+    uhred,phred=xh_red
+    Uh_red,Ph_red=Xh_red
 
 
-#     uhred2 = GridapDistributed.redistribute_fe_function(uh,Vhred,fmodel_red,red_glue)
-#     e = u - uhred2
-#     el2 = sqrt(sum( ∫( e*e )*dΩhred ))
-#     println("[REDISTRIBUTE SOLUTION] el2 < tol: $(el2) < $(tol)")
-#     @assert el2 < tol
+    trian = Triangulation(fmodel_red)
+    degree = 2*(order+1)
+    dΩhred = Measure(trian,degree)
 
-#     fmodel_red
-#   end
+    u_ex, p_ex, f_ex=get_analytical_functions(Dc)
 
-#   function test_refine_and_coarsen_at_once(ranks,
-#             dmodel::OctreeDistributedDiscreteModel{Dc},
-#             order) where Dc
-#     u(x) = x[1]+x[2]^order
-#     f(x) = -Δ(u)(x)
-#     degree = 2*order+1
-#     ref_coarse_flags=map(ranks,partition(get_cell_gids(dmodel.dmodel))) do rank,indices
-#         flags=zeros(Cint,length(indices))
-#         flags.=nothing_flag        
-#         if (rank==1)
-#            flags[1:2^Dc].=coarsen_flag
-#         else 
-#            flags[own_length(indices)]=refine_flag
-#         end 
-#         flags
-#     end
-#     fmodel,glue=adapt(dmodel,ref_coarse_flags);
 
-#     reffe=ReferenceFE(lagrangian,Float64,order)
-#     VH=FESpace(dmodel,reffe,conformity=:H1;dirichlet_tags="boundary")
-#     UH=TrialFESpace(VH,u)
+    uhred2 = GridapDistributed.redistribute_fe_function(uhred,Uh_red,fmodel_red,red_glue)
+    e = u_ex - uhred2
+    el2 = sqrt(sum( ∫( e⋅e )*dΩhred ))
+    println("[REDISTRIBUTE SOLUTION] el2 < tol: $(el2) < $(tol)")
+    @assert el2 < tol
 
-#     Vh=FESpace(fmodel,reffe,conformity=:H1;dirichlet_tags="boundary")
-#     Uh=TrialFESpace(Vh,u)
-#     ΩH  = Triangulation(dmodel)
-#     dΩH = Measure(ΩH,degree)
+    fmodel_red
+  end
 
-#     aH(u,v) = ∫( ∇(v)⊙∇(u) )*dΩH
-#     bH(v) = ∫(v*f)*dΩH
+  function test_refine_and_coarsen_at_once(ranks,
+            dmodel::OctreeDistributedDiscreteModel{Dc},
+            order) where Dc
+    degree = 2*order+1
+    ref_coarse_flags=map(ranks,partition(get_cell_gids(dmodel.dmodel))) do rank,indices
+        flags=zeros(Cint,length(indices))
+        flags.=nothing_flag        
+        if (rank==1)
+           flags[1:2^Dc].=coarsen_flag
+        else 
+           flags[own_length(indices)]=refine_flag
+        end 
+        flags
+    end
+    fmodel,glue=adapt(dmodel,ref_coarse_flags);
 
-#     op = AffineFEOperator(aH,bH,UH,VH)
-#     uH = solve(op)
-#     e = u - uH
+    # Solve coarse
+    xH,XH=solve_darcy(dmodel,order)
+    check_error_darcy(dmodel,order,xH)
+    uH,pH=xH
 
-#     # # Compute errors
-#     el2 = sqrt(sum( ∫( e*e )*dΩH ))
-#     eh1 = sqrt(sum( ∫( e*e + ∇(e)⋅∇(e) )*dΩH ))
+    # Solve fine
+    xh,Xh=solve_darcy(fmodel,order)
+    check_error_darcy(fmodel,order,xh)
+    uh,ph=xh
 
-#     tol=1e-7
-#     @assert el2 < tol
-#     @assert eh1 < tol
+    # prolongation via interpolation
+    Uh,Ph=Xh
+    uHh=interpolate(uH,Uh)
+    e = uh - uHh
 
-#     Ωh  = Triangulation(fmodel)
-#     dΩh = Measure(Ωh,degree)
+    trian = Triangulation(fmodel)
+    degree = 2*(order+1)
+    dΩh = Measure(trian,degree)
 
-#     ah(u,v) = ∫( ∇(v)⊙∇(u) )*dΩh
-#     bh(v) = ∫(v*f)*dΩh
-
-#     op = AffineFEOperator(ah,bh,Uh,Vh)
-#     uh = solve(op)
-#     e = u - uh
-
-#     # # Compute errors
-#     el2 = sqrt(sum( ∫( e*e )*dΩh ))
-#     eh1 = sqrt(sum( ∫( e*e + ∇(e)⋅∇(e) )*dΩh ))
-
-#     tol=1e-7
-#     @assert el2 < tol
-#     @assert eh1 < tol
-
-#     # prolongation via interpolation
-#     uHh=interpolate(uH,Uh)
-#     e = uh - uHh
-#     el2 = sqrt(sum( ∫( e*e )*dΩh ))
-#     tol=1e-7
-#     @assert el2 < tol
-#   end
+    el2 = sqrt(sum( ∫( e⋅e )*dΩh ))
+    tol=1e-7
+    @assert el2 < tol
+  end
 
   function test_2d(ranks,order)
     coarse_model=CartesianDiscreteModel((0,1,0,1),(1,1))
@@ -243,8 +175,8 @@ module DarcyNonConformingOctreeModelsTests
   u_ex_2D(x) = VectorValue(2*x[1],x[1]+x[2])
   p_ex_2D(x) = x[1]-x[2]
   f_ex_2D(x) = u_ex_2D(x) + ∇(p_ex_2D)(x)
-  u_ex_3D(x) = VectorValue(x[1],x[1]+x[2],2*x[3])
-  p_ex_3D(x) = 3.14
+  u_ex_3D(x) = VectorValue(x[1],x[2],x[3])
+  p_ex_3D(x) = x[2]
   f_ex_3D(x) = u_ex_3D(x) + ∇(p_ex_3D)(x)
   
   function get_analytical_functions(Dc)
@@ -270,7 +202,7 @@ module DarcyNonConformingOctreeModelsTests
     else
       @assert Dc==3
       dirichlet_tags=[21,22,23]
-      neumanntags = [24,25,26]
+      neumanntags=[24,25,26]
     end 
 
     u_ex, p_ex, f_ex=get_analytical_functions(Dc)
@@ -302,9 +234,24 @@ module DarcyNonConformingOctreeModelsTests
     
     a((u, p),(v, q)) = ∫(u⋅v)dΩ +∫(q*(∇⋅u))dΩ-∫((∇⋅v)*p)dΩ
     b(( v, q)) = ∫( v⋅f_ex + q*(∇⋅u_ex))dΩ - ∫((v⋅nb)*p_ex )dΓ
-    
+
+    # xh=get_trial_fe_basis(X)
+    # yh=get_fe_basis(Y)
+    # vh, qh = yh
+    # uh, ph = xh 
+
+    # res=assemble_vector(a((u_ex,p_ex),(vh,qh))-b((vh,qh)),Y)
+    # println("res: $(norm(res))")
+
+    # axv=assemble_vector(a((u_ex,p_ex),(vh,qh)),Y)
+    # bv=assemble_vector(b((vh,qh)),Y)
+    # map(axv.vector_partition,bv.vector_partition) do axv_vp, bv_vp 
+    #    println(axv_vp-bv_vp)
+    # end
+
     op = AffineFEOperator(a,b,X,Y)
     xh = solve(op)
+    xh,X
   end 
 
   function check_error_darcy(model::GridapDistributed.DistributedDiscreteModel{Dc},order,xh) where {Dc}
@@ -312,7 +259,6 @@ module DarcyNonConformingOctreeModelsTests
     degree = 2*(order+1)
     dΩ = Measure(trian,degree)
 
-    uh = xh[1]
     uh, ph = xh
 
     u_ex, p_ex, f_ex = get_analytical_functions(Dc)
@@ -352,15 +298,18 @@ module DarcyNonConformingOctreeModelsTests
     # global_logger(debug_logger); # Enable the debug logger globally
     np    = MPI.Comm_size(MPI.COMM_WORLD)
     ranks = distribute(LinearIndices((np,)))
+
+    order=1
+    test_2d(ranks,order)
     
     # domain = (0,1,0,1)
     # order = 1
     # coarse_model=CartesianDiscreteModel(domain,(1,1))
     # driver(ranks,coarse_model,order)
 
-    domain = (0,1,0,1,0,1)
-    order = 1
-    coarse_model=CartesianDiscreteModel(domain,(1,1,1))
-    driver(ranks,coarse_model,order)
+    # domain = (0,1,0,1,0,1)
+    # order = 2
+    # coarse_model=CartesianDiscreteModel(domain,(1,1,1))
+    # driver(ranks,coarse_model,order)
   end
 end
