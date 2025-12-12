2d726e3499c374873f6ac2b09972314a585e5d98
Alberto F. Martin <alberto.f.martin@anu.edu.au>
2025-12-12 18:07:17 +1100

    BUG in generate_dg_operator work-arounded

--- test/PoissonNonConformingOctreeModelsTests.jl
+++ test/PoissonNonConformingOctreeModelsTests.jl
@@ -99,7 +99,7 @@ module PoissonNonConformingOctreeModelsTests
          # i.e., no parenthesis around jump(...)*jump(...). I have not been
          # able to figure out why, but has to do with the scaling applied to the
          # first jump(...)
-         ∫( (γ/h)*jump(v*n_ΛH)⋅jump(u*n_ΛH) -
+         ∫( (γ/h)*(jump(v*n_ΛH)⋅jump(u*n_ΛH)) -
          jump(v*n_ΛH)⋅mean(∇(u)) -
          mean(∇(v))⋅jump(u*n_ΛH) )*dΛH
 
@@ -542,35 +542,33 @@ module PoissonNonConformingOctreeModelsTests
     # debug_logger = ConsoleLogger(stderr, Logging.Debug)
     # global_logger(debug_logger); # Enable the debug logger globally
     ranks = distribute(LinearIndices((MPI.Comm_size(MPI.COMM_WORLD),)))
-    # for Dc=2:3, perm in (1,2,4), order=(1,2), scalar_or_vector in (:scalar,)
-    #   test(ranks,Val{Dc},perm,order,:dg,_field_type(Val{Dc}(),scalar_or_vector))
-    # end
-    # for Dc=2:3, perm in (1,2), order in (1,4), scalar_or_vector in (:vector,)
-    #  test(ranks,Val{Dc},perm,order,:cg,_field_type(Val{Dc}(),scalar_or_vector))
-    # end
+    for Dc=2:3, perm in (1,2,4), order=(1,2), scalar_or_vector in (:scalar,)
+      test(ranks,Val{Dc},perm,order,:dg,_field_type(Val{Dc}(),scalar_or_vector))
+    end
+    for Dc=2:3, perm in (1,2), order in (1,4), scalar_or_vector in (:vector,)
+     test(ranks,Val{Dc},perm,order,:cg,_field_type(Val{Dc}(),scalar_or_vector))
+    end
     for order=2:2, scalar_or_vector in (:scalar,), num_ghost_layers in (1,2)
       if (num_ghost_layers==1 || length(ranks)>1)
         test_2d(ranks,order,:dg,_field_type(Val{2}(),scalar_or_vector), num_amr_steps=5,
                 num_ghost_layers=num_ghost_layers)
-        # test_3d(ranks,order,:dg,_field_type(Val{3}(),scalar_or_vector), num_amr_steps=4,
-        #         num_ghost_layers=num_ghost_layers)
+        test_3d(ranks,order,:dg,_field_type(Val{3}(),scalar_or_vector), num_amr_steps=4,
+                 num_ghost_layers=num_ghost_layers)
       end
     end
-
-    # for order=1:2, scalar_or_vector in (:scalar,), num_ghost_layers in (1,), cg_or_dg in (:dg,:cg)
-    #    test_2d_fe_space_on_triangulation(ranks,
-    #                                      order,
-    #                                      cg_or_dg,
-    #                                      _field_type(Val{2}(),scalar_or_vector),
-    #                                      num_amr_steps=5,
-    #                                      num_ghost_layers=num_ghost_layers)
-    # end
-
-    # for order=2:2, scalar_or_vector in (:scalar,:vector), num_ghost_layers in (1,)
-    #   test_2d(ranks,order,:cg,_field_type(Val{2}(),scalar_or_vector), num_amr_steps=5,
-    #            num_ghost_layers=num_ghost_layers)
-    #   test_3d(ranks,order,:cg,_field_type(Val{3}(),scalar_or_vector), num_amr_steps=4,
-    #           num_ghost_layers=num_ghost_layers )
-    # end
+    for order=1:2, scalar_or_vector in (:scalar,), num_ghost_layers in (1,), cg_or_dg in (:dg,:cg)
+       test_2d_fe_space_on_triangulation(ranks,
+                                         order,
+                                         cg_or_dg,
+                                         _field_type(Val{2}(),scalar_or_vector),
+                                         num_amr_steps=5,
+                                         num_ghost_layers=num_ghost_layers)
+    end
+    for order=2:2, scalar_or_vector in (:scalar,:vector), num_ghost_layers in (1,)
+      test_2d(ranks,order,:cg,_field_type(Val{2}(),scalar_or_vector), num_amr_steps=5,
+               num_ghost_layers=num_ghost_layers)
+      test_3d(ranks,order,:cg,_field_type(Val{3}(),scalar_or_vector), num_amr_steps=4,
+              num_ghost_layers=num_ghost_layers )
+    end
   end
 end
