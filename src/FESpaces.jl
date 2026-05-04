19cecbd48f53fc68b1743d25e7ee583dd6f6eb42
Alberto F. Martin <alberto.f.martin@anu.edu.au>
2026-05-04 21:52:30 +1000

    Fix BUG in add_constraints(...) helper function

--- src/FESpaces.jl
+++ src/FESpaces.jl
@@ -984,7 +984,7 @@ function _add_constraints(model,
          @debug "[$(part_id(indices))]: l2g_cell_gids=$(local_to_global(indices))"
          @debug "[$(part_id(indices))]: l2o_owner=$(local_to_owner(indices))"
     end
-    vector_type = GridapDistributed._find_vector_type(spaces_w_constraints,gids)
+    vector_type = GridapDistributed._find_vector_type(spaces_w_constraints,gids; split_own_and_ghost=split_own_and_ghost)
     space = GridapDistributed.DistributedSingleFieldFESpace(spaces_w_constraints,gids,trian,vector_type)
     return GridapDistributed._add_distributed_constraint(space,cell_reffe,constraint)
 end 
